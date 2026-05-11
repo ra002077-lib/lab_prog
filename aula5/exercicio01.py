b26af95b43c674383be0126023ed9dbe674ccfde
Itamar B P Junior <002077@libertas.edu.br>
2026-05-11 20:49:53 -0300

    Delete aula5 directory
    exclusão para troca de nome

--- aula5/exercicio01.py
+++
@@ -1,9 +0,0 @@
-# Somar três números
-# entrada de dados
-a = int(input("Valor 1°: "))
-b = int(input("Valor 2°: ")) 
-c = int(input("Valor 3°: "))
-# processamento
-r = a+b+c
-# saida
-print(f"resultado da soma e': {r}")
