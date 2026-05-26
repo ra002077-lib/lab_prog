f5b6e690692c30227f3157ddf52610d186a86595
Itamar B P Junior <002077@libertas.edu.br>
2026-05-25 22:22:16 -0300

    Add files via upload

---
+++ Atividades/Listas/Exercicio04.py
@@ -0,0 +1,16 @@
+lista1 = [1, 2, 3, 4]
+lista2 = [10, 20, 30, 40, 50, 60]
+lista_intercalada = []
+
+if len(lista1) <= len(lista2):
+    menor, maior = lista1, lista2
+else:
+    menor, maior = lista2, lista1
+
+for i in range(len(maior)):
+    if (i < len(menor)):
+        lista_intercalada.append(menor[i])
+    lista_intercalada.append(maior[i])
+print(f"Lista1: {lista1}")
+print(f"Lista2: {lista2}")
+print(f"Lista intercalada: {lista_intercalada}")
